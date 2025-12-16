840241f884b6e4b9ebf2f88c6941d35a06cc8d1f
Yxonyx <kaiserjonatan911@gmail.com>
2025-12-16 14:36:10 +0100

    Remove datalake.py - not needed for Community Edition

--- cubix_data_engineer_capstone/utils/__init__.py
+++ cubix_data_engineer_capstone/utils/__init__.py
@@ -2,5 +2,4 @@
 
 from .config import Config
 from .authentication import get_spark_session
-from .databricks import read_file_from_volume
-from .datalake import DataLakeClient
+from .databricks import read_file_from_volume, write_file_to_volume
